cbbe4eb697018a5499079012e5efa7e7d9d0c6a0
Tony Francis <gtony@suyati.com>
2017-03-12 20:31:25 +0530

    react-big-calendar:

--- frontend/src/lib/Calendar.jsx
+++ frontend/src/lib/Calendar.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 import BigCalendar from 'react-big-calendar';
 import moment from 'moment';
+import CalendarCss from 'react-big-calendar/lib/css/react-big-calendar.css';
+
 // Setup the localizer by providing the moment (or globalize) Object
 // to the correct localizer.
 BigCalendar.momentLocalizer(moment);
 const Calendar = props => (
-  <div>
+  <div style={{ height: "500px"}}>
     <BigCalendar
+      views={['month']}
       events={[]}
       defaultDate={new Date(2015, 3, 1)}
     />
